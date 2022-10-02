782d1fe829639d25601607fd0fb5bb6ba93453f5
rukonbd1 <rukonbd.dev@gmail.com>
2022-10-02 22:40:50 +0600

    80% done

--- app.js
+++ app.js
@@ -3,13 +3,76 @@ const screen2= document.getElementById('screen2');
 const signs= document.getElementsByClassName('sign');
 const operators = document.getElementsByClassName('operator');
 const actions= document.getElementsByClassName("action");
-const getNumber=(sign,index,text)=>{
-  for (let i = 0; i < index.length; i++) {
-    const element = index[i];
-    console.log(element)
-    
+
+//calculation of two numbers
+const calculation=(x,y,sign)=>{
+  const isInt=(a)=>{
+    if(a.includes('.')){
+      return parseFloat(a);
+    }
+    else{
+      return parseInt(a);
+    }
+  }
+  const num1=isInt(x)
+  const num2=isInt(y)
+  if(sign=="+"){
+    return num1+num2;
+  }
+  else if(sign=="−"){
+    return num1-num2;
+  }
+  else if(sign=="×"){
+    return num1*num2;
+  }
+  else if(sign=="÷"){
+    return num1/num2;
+  }
+  else{
+    return 0;
   }
 }
+
+//operation on sign and numbers
+
+const operations=(signs,numbers)=>{
+  if(signs.length==0){
+    return screen2.innerText;
+  }
+  else if(numbers.length==2){
+    return calculation(numbers[0],numbers[1],signs[0]);
+  }
+  else if(numbers.length>2){
+    let result='';
+    for (let i = 0; i < numbers.length-1; i++) {
+      if(i==0){
+        result=calculation(numbers[0],numbers[1],signs[0])
+      }
+      else{
+        result =calculation(result,numbers[i+1],signs[i])
+      }     
+    }
+    return result;
+  }
+}
+//get number from screen
+
+const getNumber=(index,text)=>{
+  let numbers=[];
+  let previous=0;
+  for (let i = 0; i < index.length; i++) {
+    let next = index[i];
+    numbers.push(text.slice(previous,next))
+    previous=next+1;
+  }
+  let num = text.slice(previous,text.length)
+  if((num)){
+    numbers.push(num);
+  }
+  return numbers;
+}
+
+//get operrators from screen
 
 const getOperator=(arr)=>{
   let newArr=[]
@@ -22,18 +85,10 @@ const getOperator=(arr)=>{
   }
   return {newArr,indexArr};
 }
-const calculation=(value)=>{
-  let array=[];
-  for (let i = 0; i < value.length; i++) {
-    const element = value.charAt(i);
-    array.push(element) 
-  }
-  console.log(getOperator(array))
-  
-}
+
+//fix duplicate input sign and display input
 
 let text="";
-
 for (let i = 0; i < signs.length; i++) {
   let sign = signs[i];
   sign.addEventListener('click',(e)=>{ 
@@ -46,9 +101,14 @@ for (let i = 0; i < signs.length; i++) {
         text=text.slice(0,-1);  
       }
     }
-    screen2.innerText=text;
+    if(text.length<16){
+
+      screen2.innerText=text;
+    }
   })  
 }
+
+//handle event
 
 for (let i = 0; i < actions.length; i++) {
   let sign = actions[i];
@@ -56,6 +116,7 @@ for (let i = 0; i < actions.length; i++) {
     let txt= e.target.innerText;
       if(txt=="AC"){
       screen2.innerText="0"
+      screen1.innerText="0"
       text="";
       }
       if(txt=="Del"){
@@ -71,15 +132,30 @@ for (let i = 0; i < actions.length; i++) {
         text=newText;
         screen2.innerText=newText;
         }
-        
       }
       if(txt=="="){
         let operator=getOperator(text);
-        let sign=operator.newArr;
         let indexArr= operator.indexArr;
-        getNumber(sign,indexArr,text);
-
-      }
-    
+        let sign=operator.newArr;
+        let numbers = getNumber(indexArr,text);
+        let result= operations(sign,numbers);
+        if ((result)){
+          if((text)){
+            screen1.innerText=text;
+            screen2.innerText=result;
+            text=result;
+          }
+          else{
+            screen1.innerText="0";
+            screen2.innerText="0";
+            text='';
+          } 
+        }
+        else{
+          return
+        }
+      }   
   })  
 }
+
+
